ca92b7814eddf0fae9b962e41711618ff22f2aac
sebastianbarcia <sebastianbarcia.97@gmail.com>
2022-03-14 22:56:00 +0100

    reajuste js portada

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ const rubro =
 $(".colorH1").text(`${"restaurante"}`);  
 
 function intervalSet(){
-window.setTimeout(miFuncion, 1500, 'pasteleria');
-window.setTimeout(miFuncionb, 3000, 'Restaurant');
-window.setTimeout(miFuncionc, 4500, 'Bar');
-window.setTimeout(miFunciond, 6000, 'Food truck');
+window.setTimeout(miFuncion, 1500);
+window.setTimeout(miFuncionb, 3000);
+window.setTimeout(miFuncionc, 4500);
+window.setTimeout(miFunciond, 6000);
 }
-let intervalFinal = setInterval(intervalSet,6000)
+let intervalFinal = setInterval(intervalSet,6000);
 
 function miFuncion(a) { 
   $(".colorH1").text(`
