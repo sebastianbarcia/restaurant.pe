8d65288eb56a41b091d684ba6baf5c8ecee96ffe
sebastianbarcia <sebastianbarcia.97@gmail.com>
2022-04-02 01:13:12 +0200

    retoques en js e index

--- js/form.js
+++ js/form.js
@@ -1,13 +1,17 @@
-/**Deseleccionar boton "Enviar"**/
+/*****************************************/
+/*******Deseleccionar boton "Enviar"******/
+/*****************************************/
 
 /*carga la cantidad de items en los input*/
-blockButton()
+blockButton();
 
 /*variables globales*/
 let names = document.querySelector(".name");
 let telephone = document.querySelector(".telephone");
 let mail = document.querySelector(".mail");
 let local = document.querySelector(".local");  
+
+let correo = mail.value;
 
 /*Los eventos chequean si los inputs estan completos*/
 names.onchange = () => {
@@ -19,29 +23,39 @@ telephone.onchange = () => {
 }
 mail.onchange = () => {
     blockButton();
+    validarEmail(mail);
 }
 local.onchange = () => {
     blockButton();
 }
 
 /*funcion para deseleccionar boton*/
-function blockButton(){ 
-    
+
+function blockButton(){     
 let names = document.querySelector(".name").value;
 let telephone = document.querySelector(".telephone").value;
 let mail = document.querySelector(".mail").value;
 let local = document.querySelector(".local").value;  
 
-if(((names.length) && (telephone.length) && (mail.length) && (local.length)) == 0){
+if(((names.length) && (telephone.length) && (mail.length) && (local.length)) == 0){   
     document.getElementById('button').disabled = true;
     }
     else if (((names.length) && (telephone.length) && (mail.length) && (local.length)) >= 1){
-        document.getElementById('button').disabled = false; 
+    document.getElementById('button').disabled = false; 
     }
 }
 
-/*funcion para cargar datos en el modal*/
+/*******funcion para validar email *******/
+function validarEmail(mail) {
+    if (/\S+@\S+/.test(mail.value)){
+        console.log("mail correcto");
+    } else {
+     alert("Algo no ha ido bien, introduzca su email correctamente");
+     document.getElementById('button').disabled = true;
+    }
+  }
 
+/*funcion para cargar datos en el modal*/
 function cargarName(){
 $(".modal-title").text(`Mensaje enviado!`)
 $(".parrafoModal").text(`${names.value}, ten cerca las vias de contacto que nos proporcionastes, te llamaremos pronto!`)
